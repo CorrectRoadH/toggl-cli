f4c34ba7a3ff8e4516cb54b7235f85a5924c1cd7
CorrectRoadH <29306285+CorrectRoadH@users.noreply.github.com>
2026-03-06 22:39:52 +0800

    fix: update issue link in error messages

--- src/constants.rs
+++ src/constants.rs
@@ -1,11 +1,10 @@
-pub const OUTDATED_APP_ERROR_MESSAGE: &str =
-    "Make sure you are on the latest version of the app or file an issue here:";
-pub const CLIENT_NAME: &str = "github.com/watercooler-labs/toggl-cli/toggl-cli";
+pub const OUTDATED_APP_ERROR_MESSAGE: &str = "If this keeps happening, please file an issue here:";
+pub const CLIENT_NAME: &str = "github.com/CorrectRoadH/toggl-cli/toggl-cli";
 pub const GENERIC_ERROR: &str = "Something went wrong.";
 pub const NETWORK_ERROR_MESSAGE: &str =
     "An error occurred when making a network request\nCheck your connection and try again.";
 pub const DESERIALIZATION_ERROR_MESSAGE: &str = "An error occurred when making a network request.";
-pub const ISSUE_LINK: &str = "https://github.com/watercooler-labs/toggl-cli/issues/new";
+pub const ISSUE_LINK: &str = "https://github.com/CorrectRoadH/toggl-cli/issues/new";
 pub const CREDENTIALS_ACCESS_ERROR: &str =
     "An unknown error occurred while reading your credentials.";
 pub const CREDENTIALS_WRITE_ERROR: &str =
